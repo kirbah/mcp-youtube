46fef39aed3f61252b1b0e576549645cf52eb39d
Kiryl Bahdanau <kirbah@gmail.com>
2025-06-16 14:08:27 +0200

    feat: enhance ToolDefinition interface and handlers to support type safety with specific parameters

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -38,70 +38,92 @@ import {
 import { isEnabled } from "../utils/featureFlags.js";
 
 import type { YoutubeService } from "../services/youtube.service.js";
-import type { CacheService } from "../services/cache.service.js";
+import type { CacheService } from "../services/cache.service.js"; // Re-adding as it's used in handlers
 import type { CallToolResult } from "@modelcontextprotocol/sdk/types.js";
 import type { IServiceContainer } from "../container.js";
+import type {
+  VideoDetailsParams,
+  SearchParams,
+  TranscriptsParams,
+  ChannelStatisticsParams,
+  ChannelParams,
+  TrendingParams,
+  VideoCategoriesParams,
+  FindConsistentOutlierChannelsParams,
+} from "../types/tools.js";
+import type { FindConsistentOutlierChannelsOptions } from "../types/analyzer.types.js";
+import type { ZodRawShape } from "zod";
 
-export interface ToolDefinition {
+export interface ToolDefinition<TParams = unknown> {
   config: {
     name: string;
     description: string;
-    inputSchema: any;
+    inputSchema: ZodRawShape;
   };
   handler:
-    | ((params: any, videoManager: YoutubeService) => Promise<CallToolResult>)
-    | ((params: any) => Promise<CallToolResult>);
+    | ((
+        params: TParams,
+        youtubeService: YoutubeService,
+        cacheService: CacheService
+      ) => Promise<CallToolResult>)
+    | ((
+        params: TParams,
+        youtubeService: YoutubeService
+      ) => Promise<CallToolResult>)
+    | ((params: TParams) => Promise<CallToolResult>);
 }
 
 export function allTools(container: IServiceContainer): ToolDefinition[] {
   const { youtubeService, cacheService } = container;
 
-  const toolDefinitions: ToolDefinition[] = [
+  const toolDefinitions: ToolDefinition<any>[] = [
     // Video tools
     {
       config: getVideoDetailsConfig,
-      handler: (params: any) =>
+      handler: (params: VideoDetailsParams) =>
         getVideoDetailsHandler(params, youtubeService, cacheService),
-    },
+    } as ToolDefinition<VideoDetailsParams>,
     {
       config: searchVideosConfig,
-      handler: (params: any) =>
+      handler: (params: SearchParams) =>
         searchVideosHandler(params, youtubeService, cacheService),
-    },
+    } as ToolDefinition<SearchParams>,
     {
       config: getTranscriptsConfig,
-      handler: (params: any) => getTranscriptsHandler(params, youtubeService),
-    },
+      handler: (params: TranscriptsParams) =>
+        getTranscriptsHandler(params, youtubeService),
+    } as ToolDefinition<TranscriptsParams>,
     // Channel tools
     {
       config: getChannelStatisticsConfig,
-      handler: (params: any) =>
+      handler: (params: ChannelStatisticsParams) =>
         getChannelStatisticsHandler(params, youtubeService),
-    },
+    } as ToolDefinition<ChannelStatisticsParams>,
     {
       config: getChannelTopVideosConfig,
-      handler: (params: any) =>
+      handler: (params: ChannelParams) =>
         getChannelTopVideosHandler(params, youtubeService, cacheService),
-    },
+    } as ToolDefinition<ChannelParams>,
     // General tools
     {
       config: getTrendingVideosConfig,
-      handler: (params: any) =>
+      handler: (params: TrendingParams) =>
         getTrendingVideosHandler(params, youtubeService),
-    },
+    } as ToolDefinition<TrendingParams>,
     {
       config: getVideoCategoriesConfig,
-      handler: (params: any) =>
+      handler: (params: VideoCategoriesParams) =>
         getVideoCategoriesHandler(params, youtubeService, cacheService),
-    },
+    } as ToolDefinition<VideoCategoriesParams>,
   ];
 
   // Add feature-flagged tools conditionally
   if (isEnabled("toolFindConsistentOutlierChannels")) {
     toolDefinitions.push({
       config: findConsistentOutlierChannelsConfig,
-      handler: findConsistentOutlierChannelsHandler,
-    });
+      handler: (params: FindConsistentOutlierChannelsOptions) =>
+        findConsistentOutlierChannelsHandler(params),
+    } as ToolDefinition<FindConsistentOutlierChannelsOptions>);
   }
 
   return toolDefinitions;
